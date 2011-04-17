c0e292ab1b83b12bfbf0a81ee77c93a7ac5ca1a8
Canx <canchete@gmail.com>
2011-04-17 22:37:59 +0200

    Done with sign in

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,25 +7,29 @@ Spork.prefork do
   # need to restart spork for it take effect.
   ENV["RAILS_ENV"] ||= 'test'
   unless defined?(Rails)
-		require File.dirname(__FILE__) + "/../config/environment"
-	end
+    require File.dirname(__FILE__) + "/../config/environment"
+  end
 
-	require 'rspec/rails'
-	
-	Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+  require 'rspec/rails'
 
-	Rspec.configure do |config|
-		config.mock_with :rspec
-		config.fixture_path = "#{::Rails.root}/spec/fixtures"
-		config.use_transactional_fixtures = true
-		
-		ActiveSupport::Dependencies.clear 
-	end
+  Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+
+  Rspec.configure do |config|
+    config.mock_with :rspec
+    config.fixture_path = "#{::Rails.root}/spec/fixtures"
+    config.use_transactional_fixtures = true
+
+    ActiveSupport::Dependencies.clear 
+
+    def test_sign_in(user)
+      controller.sign_in(user)
+    end
+  end
 end
 
 Spork.each_run do
   # This code will be run each time you run your specs.
-  
+
 end
 
 # --- Instructions ---
@@ -68,4 +72,5 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
+
 end
