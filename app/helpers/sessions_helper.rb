9e6880208de00bcb9a3f563e6b3b122a36393b1d
Canx <canchete@gmail.com>
2011-04-24 20:40:13 +0200

    forwarding url session

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,13 +1,11 @@
 module SessionsHelper
 
   def sign_in(user)
-    #cookies.permanent.signed[:remember_token] = [user.id, user.salt]
     session[:remember_token] = [user.id, user.salt]
     current_user = user
   end
 
   def sign_out
-    #cookies.delete(:remember_token)
     reset_session
     current_user = nil
   end
@@ -24,6 +22,20 @@ module SessionsHelper
     !current_user.nil?
   end
 
+  def current_user?(user)
+    user == current_user
+  end
+
+  def deny_access
+    store_location
+    redirect_to signin_path, :notice => "Please sign in to access this page."
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    clear_return_to
+  end
+
   private
 
     def user_from_remember_token
@@ -31,7 +43,14 @@ module SessionsHelper
     end
 
     def remember_token
-      #cookies.signed[:remember_token] || [nil, nil]
       session[:remember_token] || [nil, nil]
+    end
+
+    def store_location
+      session[:return_to] = request.fullpath
+    end
+
+    def clear_return_to
+      session[:return_to] = nil
     end
 end
