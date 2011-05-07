50b772a4679389a0d78ad09b636bdba57c8dc66a
Canx <canchete@gmail.com>
2011-05-07 19:38:48 +0200

    Done with user edit/update, index, and destroy actions

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,9 @@
 Factory.define :user do |user|
-  user.name                  "Ruben Cancho"
-  user.email                 "canchete@gmail.com"
+  user.name                  "Example User"
+  user.email                 "example@railstutorial.org"
   user.password              "foobar"
   user.password_confirmation "foobar"
+  user.admin                 false
 end
 
 Factory.sequence :email do |n|
